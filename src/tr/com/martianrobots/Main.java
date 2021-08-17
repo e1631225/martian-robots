25639f661ba3b8ed9ca6a079240e36a7a38f11ee
suleymanMac <suleymanyilmazs@gmail.com>
2021-08-17 21:11:51 +0300

    Input logic is implemented

--- src/tr/com/martianrobots/Main.java
+++ src/tr/com/martianrobots/Main.java
@@ -1,9 +1,28 @@
 package tr.com.martianrobots;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s);
+
+        String[] mapBorderStringArray = s.split(" ");
+        int boundX = Integer.parseInt(mapBorderStringArray[0]);
+        int boundY = Integer.parseInt(mapBorderStringArray[1]);
+
+        System.out.println("x: " + boundX + " y: " + boundY);
+
+        s = br.readLine();
+        String[] initialPositionStringArray = s.split(" ");
+        System.out.println(s);
+
+        s = br.readLine();
+        char[] commandList = s.toCharArray();
+        System.out.println(s);
     }
 }
