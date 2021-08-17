46e02f6e04ea4efbffa304c3263068a15035ff1d
suleymanMac <suleymanyilmazs@gmail.com>
2021-08-17 21:15:05 +0300

    Constants are defined in Constants.java class.

---
+++ src/tr/com/martianrobots/Constants.java
@@ -0,0 +1,7 @@
+package tr.com.martianrobots;
+
+public class Constants {
+    public static final int MAX_COORDINATE_VALUE = 50;
+    public static final int MAX_INSTRUCTION_STRING_LENGTH = 100;
+    public static final String LOST = "LOST";
+}
